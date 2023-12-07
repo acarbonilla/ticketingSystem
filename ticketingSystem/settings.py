129a61d9a6fde0b6236c73bc1f0d287d4be22556
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-07 20:01:49 +0800

    Wishing it will work

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-4siu7)#i0jl7=s3_!8)h2zrmr-l4omz_^o_-^&f_@_y4-89i3e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
-
+# ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOST = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -145,6 +145,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# This is for railway setup
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -192,4 +196,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
-
