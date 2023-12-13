e71715f89ce965bbb11abf94adb1d0d1c63beef3
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-13 10:33:48 +0800

    Ticketing System

--- staff/views.py
+++ staff/views.py
@@ -87,7 +87,7 @@ def openTicket(request):
 @login_required(login_url='account_login')
 def closedTicket(request):
     # Service Desk only
-    sd = request.user.groups.filter(name="Service_Desk")
+    sd = request.user.groups.filter(name="Service_Desk, Deskside")
     if not sd:
         return redirect("authView")
 
