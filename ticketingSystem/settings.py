67d0c361ca6d019f7c2538b54c3c20714dc8d39a
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-16 13:23:07 +0800

    Ticketing System

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -214,11 +214,11 @@ CSRF_COOKIE_SECURE = True
 # Email password reset
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'carbonillaat@yahoo.com'
+EMAIL_HOST_USER = 'carbonillaalfredo@gmail.com'
 EMAIL_HOST_PASSWORD = 'XiaCarb@23'
 
 
