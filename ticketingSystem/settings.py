d3f01d0bab7a54d074a382dbf9e76a3a2b4bd88a
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-16 15:11:37 +0800

    Ticketing System

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -221,6 +221,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['carbonillaalfredo@gmail.com']
-EMAIL_HOST_PASSWORD = os.environ['bcaa pvtf ltdl nxcm']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
