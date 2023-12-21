f31386fd345430b1a108b5aa2a91cb90df62634b
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-21 20:59:23 +0800

    Ticketing System

--- baseApp/routing.py
+++ baseApp/routing.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
-    path('ws/chat/<str:room_names>/', consumers.ChatRoomConsumer.as_asgi()),
+    path('wss/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('wss/chat/<str:room_names>/', consumers.ChatRoomConsumer.as_asgi()),
 
 ]
 
