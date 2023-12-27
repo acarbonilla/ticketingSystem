ee7f1c47fe7f675a3d7d1f6b1462da03eacbb08a
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-27 11:02:41 +0800

    Ticketing System

--- ticketingSystem/wsgi.py
+++ ticketingSystem/wsgi.py
@@ -1,14 +1,9 @@
 
 import os
-import pathlib
-import dotenv
+
 from django.core.wsgi import get_wsgi_application
 
-CURRENT_DIR = pathlib.Path(__file__).resolve().parent
-BASES_DIR = CURRENT_DIR.parent
-ENV_FILE_PATH = BASES_DIR / ".env"
 
-dotenv.read_dotenv(str(ENV_FILE_PATH))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketingSystem.settings')
 
 application = get_wsgi_application()
