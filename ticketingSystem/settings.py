ab7353bddfe019e63d1cfd209928e657696e50a7
alfredo carbonilla <carbonillaalfredo@gmail.com>
2023-12-27 10:19:11 +0800

    Ticketing System

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4siu7)#i0jl7=s3_!8)h2zrmr-l4omz_^o_-^&f_@_y4-89i3e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
